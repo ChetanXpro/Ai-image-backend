4d2deb269c6f8a6ef2e00ea3f0173807d4944e0b
ChetanXpro <chetanbaliyan10@gmail.com>
2022-12-12 16:06:00 +0530

    Feat: added new route

--- Routes/image.js
+++ Routes/image.js
@@ -4,5 +4,12 @@ const generateImage = require("../Controller/image");
 const router = express.Router();
 
 router.post("/generateimage", generateImage);
+router.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Api working",
+    api: "Post request -> /generateimage",
+  });
+});
 
 module.exports = router;
